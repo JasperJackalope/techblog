4c511fb3e7ad238b111d6ff70956761d2dbf6076
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-26 20:31:35 -0700

    Fixed Package.json errors

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const sequelize = require('./config/connection');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const http = require('http');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer(app);
+
+const sess = {
+  secret: 'mySecret',
+  cookie: {
+    maxAge: 3600000, // one hour in milliseconds
+    httpOnly: true,
+    secure: false,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize, // Pass the Sequelize instance here
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  server.listen(PORT, () => console.log(`Now Listening at ${PORT}!`));
+});

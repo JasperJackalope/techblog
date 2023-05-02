eb7a83d31e1dbfec72e27716ad5942f60a48f180
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-01 22:41:13 -0700

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,11 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-  // In summary, JAWSDB_URL is used in this code to provide a connection string to a remote MySQL database when the application is deployed on a cloud platform (eg HEROKU), and the fallback options are used for local development and testing.
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASS,
     {
       host: 'localhost',
       dialect: 'mysql',
